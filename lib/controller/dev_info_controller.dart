ad6d95a5a60ebdd5c3518f6d9b8346529c9aa800
abu alqasim mohamad <abolkasm11@gmail.com>
2025-04-10 05:42:25 +0200

    First

---
+++ lib/controller/dev_info_controller.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+
+import 'package:get/get.dart';
+import 'package:qasim_profile_info/core/themes/themes.dart';
+import 'package:qasim_profile_info/main.dart';
+
+class DevInfoController extends GetxController {
+  var count1 = 0.obs; // تعريف متغير ملاحظ (Observable) لتتبع عدد معين
+
+  // تعريف متغير
+  //isDark
+  // لمراقبة حالة الثيم (مظلم أو فاتح)
+  var isDark = (sharePref?.getBool("dark") ?? false).obs;
+
+  // changeTheme: دالة تقوم بتغيير الثيم بين الوضع الفاتح والمظلم
+  changeTheme() {
+    isDark.value = !isDark.value; // تبديل قيمة isDark بين true و false
+
+    // تحديث الثيم في التطبيق بناءً على قيمة isDark
+    Get.changeTheme(isDark.value ? Thems.darkTheme : Thems.lightTheme);
+
+    // حفظ الحالة الجديدة في SharedPreferences لضمان الاحتفاظ بالثيم بعد إعادة تشغيل التطبيق
+    sharePref!.setBool("dark", isDark.value);
+  }
+
+  // inc1: دالة لزيادة قيمة count1
+  inc1() {
+    count1.value++; // زيادة المتغير بمقدار 1
+  }
+
+  // متغير لتحديد اللغة الافتراضية للتطبيق
+  Locale initalLang =
+      sharePref!.getString("lang") ==
+              null // التحقق مما إذا كان هناك لغة محفوظة في التخزين المحلي
+          ? Get
+              .deviceLocale! // إذا لم تكن هناك لغة محفوظة، يتم استخدام لغة الجهاز الافتراضية
+          : Locale(
+            "${sharePref!.getString("lang")}",
+          ); // إذا كانت هناك لغة محفوظة، يتم استرجاعها واستخدامها
+
+  void changeLang(String langCode) {
+    Locale locale = Locale(langCode);
+    sharePref!.setString("lang", langCode);
+    Get.updateLocale(locale);
+  }
+}
